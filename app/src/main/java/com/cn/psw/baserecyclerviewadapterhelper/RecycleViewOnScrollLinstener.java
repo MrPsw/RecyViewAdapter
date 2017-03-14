9b73e9d59a40481dc934e431f0847b8cfbfbfceb
MrPsw <1553715143@qq.com>
2017-03-14 15:54:11 +0800

    commit

--- app/src/main/java/com/cn/psw/baserecyclerviewadapterhelper/RecycleViewOnScrollLinstener.java
+++ app/src/main/java/com/cn/psw/baserecyclerviewadapterhelper/RecycleViewOnScrollLinstener.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
